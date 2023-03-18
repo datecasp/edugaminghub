efeefee94496c409b6be3f78783896623bcd5741
datecasp <ciudadano.fake@gmail.com>
2023-03-18 22:26:58 +0100

    added gameInstructions component. No func

---
+++ src/app/game-instructions/game-instructions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-instructions',
+  templateUrl: './game-instructions.component.html',
+  styleUrls: ['./game-instructions.component.css'],
+})
+export class GameInstructionsComponent {}
