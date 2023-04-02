b1c03eea38dcdf05e9667c0704e21c2b995c374b
datecasp <ciudadano.fake@gmail.com>
2023-04-02 03:47:51 +0200

    added authGuard to games routes

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,41 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import {
+  Router,
+  NavigationExtras,
+} from '@angular/router';
+import { AuthService } from '../services/auth-service.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+/**
+ * Logic to check if a user can navigate to different routes
+ * ATM logged users can navigate to contacts/ Not looged users can´t
+ * authGuard is used as flag in app-routing.module paths
+ * @returns 
+ */
+export const authGuard = () => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  if (authService.isLoggedIn) {
     return true;
   }
-  
-}
+
+  // Create a dummy session id
+  const sessionId = 123456789;
+
+  // Set our navigation extras object
+  // that contains our global query params and fragment
+  const navigationExtras: NavigationExtras = {
+    queryParams: { session_id: sessionId },
+    fragment: 'anchor'
+  };
+
+  // Navigate to the login page with extras
+  return router.createUrlTree(['/login'], navigationExtras);
+};
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
